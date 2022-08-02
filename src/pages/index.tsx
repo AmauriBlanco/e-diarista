12867597dab82780f1658cf1994cab6107507cef
AmauriBlanco <blancoamauri@gmail.com>
2022-08-01 21:27:00 -0300

    semana 11 aula 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,24 @@
-import type { GetStaticProps, NextPage } from "next";
-import Advantages from "ui/partials/index/_advantages";
-import FrequentQuestion from "ui/partials/index/_frequent-question";
-import Presentation from "ui/partials/index/_presentation";
+import type { GetStaticProps, NextPage } from 'next';
+import Advantages from 'ui/partials/index/_advantages';
+import FrequentQuestion from 'ui/partials/index/_frequent-question';
+import Presentation from 'ui/partials/index/_presentation';
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
-      title: "",
+      title: '',
     },
   };
 };
 
 const Index: NextPage<{ title: string }> = (props) => {
-  return <div>
-    <Presentation/>
-    <Advantages/>
-    <FrequentQuestion/>
-  </div>;
+  return (
+    <div>
+      <Presentation />
+      <Advantages />
+      <FrequentQuestion />
+    </div>
+  );
 };
 
 export default Index;
-
