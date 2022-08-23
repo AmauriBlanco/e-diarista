7803a8790f4e7bfda133d652aa66c681da261d20
AmauriBlanco <blancoamauri@gmail.com>
2022-08-22 22:03:53 -0300

    semana 15 - aula 9

--- src/data/hooks/useApi.hook.ts
+++ src/data/hooks/useApi.hook.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
-import { ApiService } from 'data/services/ApiService';
+import { ApiLinksInterface } from 'data/@Types/ApiLinksInterface';
+import { ApiService, ApiServiceHeteoas } from 'data/services/ApiService';
 import useSWR from 'swr';
 
 export default function useApi<OutputType>(
@@ -10,6 +11,22 @@ export default function useApi<OutputType>(
     const reponse = await ApiService(url, config);
 
     return reponse.data;
+  });
+  return { data, error };
+}
+
+export function useApiHeteoas<OutputType>(
+  links: ApiLinksInterface[] = [],
+  nome: string | null,
+  config?: AxiosRequestConfig
+): { data: OutputType | undefined; error: Error } {
+  const { data, error } = useSWR<OutputType>(nome, async (nome) => {
+    return new Promise<OutputType>((resolve) => {
+      ApiServiceHeteoas(links, nome, async (request) => {
+        const response = await request<OutputType>(config);
+        resolve(response.data);
+      });
+    });
   });
   return { data, error };
 }
