0368c867035d5b0e3914729bd59cd0c7f52ab0f0
AmauriBlanco <blancoamauri@gmail.com>
2022-09-05 20:00:19 -0300

    semana 18 - aula 5

--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Divider, Typography } from '@mui/material';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import useOportunidades from 'data/hooks/pages/useOportunidades.page';
 import DataList from 'ui/components/data-display/DataList/DataList';
@@ -10,6 +10,9 @@ import Table, {
   TablePagination,
   TableRow,
 } from 'ui/components/data-display/Table/Table';
+import Dialog from 'ui/components/feedback/Dialog/Dialog';
+import JobInformation from 'ui/components/data-display/JobInformation/JobInformation';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 //import { Component } from '@styles/pages/oportunidades.styled';
 
@@ -116,14 +119,57 @@ const Oportunidades: React.FC = () => {
                 </TableRow>
               )}
             />
-            <TablePagination 
-              count={totalPage} page={currentPage} onChange={(event, nextPage)=> setCurrentPage(nextPage)}
+            <TablePagination
+              count={totalPage}
+              page={currentPage}
+              onChange={(event, nextPage) => setCurrentPage(nextPage)}
             />
           </>
         )
       ) : (
         <Typography align="center">Nenhuma oportunidade ainda</Typography>
       )}
+      <Dialog
+        isOpen={false}
+        onClose={() => {}}
+        title={'Se candidatar à diária'}
+        subtitle={'Tem certeza que deseja se candidatar a diária abaixo?'}
+      >
+        <div>
+          <JobInformation>
+            <>
+              <div>
+                Data:<strong>01/01/2022</strong>
+              </div>
+              <div>Enderço:</div>
+              <div>
+                Valor: <strong>R$ 140,00</strong>
+              </div>
+            </>
+          </JobInformation>
+        </div>
+        <UserInformation name="Amauri" rating={3} picture={''} />
+        <Divider />
+        <Typography sx={{ p: 3, fontWeight: 'medium', bgcolor: 'grey.50' }}>
+          Últimas avaliações do cliente
+        </Typography>
+        <UserInformation
+          name="Amauri"
+          rating={3}
+          picture={''}
+          isRating={true}
+        />
+        <Typography
+          sx={{ py: 2 }}
+          variant={'subtitle2'}
+          color={'textSecundary'}
+        >
+          Ao se candidatar você ainda não é o(a) diarista escolhido(a) para
+          realizar o trabalho. Vamos analisar suas qualificações e a distância
+          para o local da diária. Caso você seja a pessoa selecionada, receberá
+          um email avisando. Atente-se à sua caixa de entrada!
+        </Typography>
+      </Dialog>
     </Container>
   );
 };
